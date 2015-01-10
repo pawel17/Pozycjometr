2cc3c189ebb51ee2edc4b865fac097ef31384c20
lukasz00 <cisowski.lukasz@gmail.com>
2015-01-10 17:58:34 +0100

    Wrócono do AutoCheck = false dla radio buttona

--- Kod/UserInterface/UserInterface/ApplicationWindow.Designer.cs
+++ Kod/UserInterface/UserInterface/ApplicationWindow.Designer.cs
@@ -360,6 +360,7 @@
             // 
             // rotationRadioButton
             // 
+            this.rotationRadioButton.AutoCheck = false;
             this.rotationRadioButton.AutoSize = true;
             this.rotationRadioButton.Location = new System.Drawing.Point(7, 17);
             this.rotationRadioButton.Name = "rotationRadioButton";
